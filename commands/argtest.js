4da128bae396db367d2423dafeb8a6573c7d1e46
Deniied <entity493@gmail.com>
2021-08-08 11:11:51 -0400

    Update argtest.js

--- commands/argtest.js
+++ commands/argtest.js
@@ -4,10 +4,6 @@ module.exports = {
     name: "argtest",
     description: 'Command for testing arguments.',
     execute(message, args) {
-        let commands = message.client.commands.array();
-
-        inv.setTimestamp();
-
         return message.channel.send(args[1]).catch(console.error);
     }
 };
