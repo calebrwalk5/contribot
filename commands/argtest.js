9bc190b04752d147d7191cde1d8cf0871349454c
Deniied <entity493@gmail.com>
2021-08-08 11:15:48 -0400

    Delete argtest.js

--- commands/argtest.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports = {
-    name: "argtest",
-    description: 'Command for testing arguments.',
-    execute(message, args) {
-        return message.channel.send(args[0]).catch(console.error);
-    }
-};
