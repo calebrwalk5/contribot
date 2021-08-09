a6ed6d44d5de4f4562609285b53e75eabba1fd14
D9ED <entity493@gmail.com>
2021-08-09 11:59:03 -0400

    Update lastrun.js

--- commands/lastrun.js
+++ commands/lastrun.js
@@ -5,15 +5,9 @@ module.exports = {
     description: 'Gets when the bot was last restarted',
     execute(message, args) {
 
-        var date = new Date(process.env.TimeStarted * 1000);
-        var hours = date.getHours();
-        var minutes = "0" + date.getMinutes();
-        var seconds = "0" + date.getSeconds();
-        var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-
         let LastRestartEmbed = new Discord.MessageEmbed()
             .setTitle('Last restart')
-            .setDescription('Bot last restarted at\nunix time stamp: ' + process.env.TimeStarted + '\nTime: ' + formattedTime)
+            .setDescription('Bot last restarted at\nunix time stamp: ' + process.env.TimeStarted)
             .setColor(process.colors.DARKER_GRAY)
             .setFooter("Results are shown in " + Intl.DateTimeFormat().resolvedOptions().timeZone + " timezone.");
 
