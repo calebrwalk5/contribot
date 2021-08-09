3a6e0ae2f4d694e5cf6fb2038c8ce28c29f5fc93
D9ED <entity493@gmail.com>
2021-08-09 11:20:56 -0400

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -9,21 +9,21 @@ module.exports = {
 
         if (!member && isNaN(args[0])) {
             member = client.users.fetch(args[0])
+
+            if (!message.member.hasPermission('BAN_MEMBERS')) return message.reply('You cannot ban members')
+            if (!member) return message.reply("Please mention a valid member of this server");
+            if (!member.kickable) return message.reply("I cannot ban this member!");
+
+            member.ban();
+
+            let BanResp = new Discord.MessageEmbed()
+                .setTitle('Banned')
+                .setDescription('Banned user ' + args[0])
+                .setColor(process.colors.DARKER_GRAY);
+
+            BanResp.setTimestamp();
+
+            return message.channel.send(BanResp).catch(console.error);
         }
-
-        if (!message.member.hasPermission('BAN_MEMBERS')) return message.reply('You cannot ban members')
-        if (!member) return message.reply("Please mention a valid member of this server");
-        if (!member.kickable) return message.reply("I cannot ban this member!");
-
-        member.ban();
-
-        let BanResp = new Discord.MessageEmbed()
-            .setTitle('Banned')
-            .setDescription('Banned user ' + args[0])
-            .setColor(process.colors.DARKER_GRAY);
-
-        BanResp.setTimestamp();
-
-        return message.channel.send(BanResp).catch(console.error);
     }
 };
