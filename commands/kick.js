2a6a2d7ded90813e401860ed13913526e805c392
D9ED <entity493@gmail.com>
2021-08-09 11:04:35 -0400

    Create kick.js

---
+++ commands/kick.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "kick",
+    description: 'Kick a user',
+    execute(message, args) {
+
+        let member = message.mentions.members.first();
+        if (!message.member.hasPermission('KICK_MEMBERS')) return message.reply('You cannot kick members')
+        if (!member) return message.reply("Please mention a valid member of this server");
+        if (!member.kickable) return message.reply("I cannot kick this member!");
+
+        member.kick();
+
+        let kickResp = new Discord.MessageEmbed()
+            .setTitle('Kicked')
+            .setDescription('Kicked user ' + args[0])
+            .setColor(process.colors.DARKER_GRAY);
+
+        kickResp.setTimestamp();
+
+        return message.channel.send(kickResp).catch(console.error);
+    }
+};
