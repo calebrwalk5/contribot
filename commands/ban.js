158a8cad320220e74c08c6ab61b953a86c2bbcd6
D9ED <entity493@gmail.com>
2021-08-09 11:11:56 -0400

    Create ban.js

---
+++ commands/ban.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "ban",
+    description: 'Ban a user',
+    execute(message, args) {
+
+        let member = message.mentions.members.first();
+        if (!message.member.hasPermission('BAN_MEMBERS')) return message.reply('You cannot ban members')
+        if (!member) return message.reply("Please mention a valid member of this server");
+        if (!member.kickable) return message.reply("I cannot ban this member!");
+
+        message.members.mentions.first().ban();
+
+        let BanResp = new Discord.MessageEmbed()
+            .setTitle('Banned')
+            .setDescription('Banned user ' + args[0])
+            .setColor(process.colors.DARKER_GRAY);
+
+        BanResp.setTimestamp();
+
+        return message.channel.send(BanResp).catch(console.error);
+    }
+};
