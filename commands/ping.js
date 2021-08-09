99eaa8f6715cf9734753e32e8422a20f68b7e867
D9ED <entity493@gmail.com>
2021-08-09 11:30:15 -0400

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "ping",
+    description: 'A ping command',
+    execute(message, args) {
+        return message.reply("Pong!").catch(console.error);
+    }
+};
