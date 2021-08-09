df297ba097a28354d7f183156d2620c566322bb9
D9ED <entity493@gmail.com>
2021-08-09 12:07:04 -0400

    almost forgot about kick

--- commands/kick.js
+++ commands/kick.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports = {
     name: "kick",
-    description: 'Kick a user',
+    description: 'Kick a user.',
     execute(message, args) {
 
         let member = message.mentions.members.first();
