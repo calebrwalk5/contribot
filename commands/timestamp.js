771312da1ff690949935dea14914f583ce4a1217
D9ED <entity493@gmail.com>
2021-08-09 12:00:45 -0400

    Create timestamp.js

---
+++ commands/timestamp.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "timestamp",
+    description: 'Gets the current unix time stamp',
+    execute(message, args) {
+
+        let LastRestartEmbed = new Discord.MessageEmbed()
+            .setTitle('Unix Time Stamp')
+            .setDescription('Current Unix Time Stamp: ' + Date.now())
+            .setColor(process.colors.DARKER_GRAY)
+            .setFooter("Results may be off by a few seconds.");
+
+        LastRestartEmbed.setTimestamp();
+
+        return message.reply(LastRestartEmbed).catch(console.error);
+    }
+};
