db427a15f0b7151cff411bdce55ab24ac4d4ca39
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2021-09-29 10:37:03 -0400

    Update anus.js

--- commands/anus.js
+++ commands/anus.js
@@ -4,6 +4,7 @@ const RICK = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 module.exports = {
     name: "anus",
     execute(message, args) {
+        return message.reply("AHHHHHHHHHHHHHHHHHHHHHH").catch(console.error);
         return message.reply(RICK).catch(console.error);
     }
 };
