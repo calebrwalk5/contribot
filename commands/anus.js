492a3bddcfcbeb69667f5b152db6ffaab526941f
HOLY SHIT ITS A FUCKING NAME <entity493@gmail.com>
2021-09-29 11:28:47 -0400

    Update anus.js

--- commands/anus.js
+++ commands/anus.js
@@ -2,9 +2,9 @@ const Discord = require("discord.js");
 const RICK = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 
 module.exports = {
-    name: "anus",
+    name: "rickroll",
+    description: 'Sends you the link for the "Never Gonna Give You Up" music video by Rick Astley',
     execute(message, args) {
-        message.reply("AHHHHHHHHHHHHHHHHHHHHHH").catch(console.error);
         return message.reply(RICK).catch(console.error);
     }
 };
